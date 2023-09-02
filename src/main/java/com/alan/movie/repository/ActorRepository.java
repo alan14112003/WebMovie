4bb92333843b26fc1cda250059d8e69729923ac3
alan14112003 <sonnn.21it@gmil.com>
2023-09-02 17:52:34 +0700

    update query

--- src/main/java/com/alan/movie/repository/ActorRepository.java
+++ src/main/java/com/alan/movie/repository/ActorRepository.java
@@ -4,9 +4,11 @@ import com.alan.movie.Model.Actor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ActorRepository extends MongoRepository<Actor, String> {
-  public Page<Actor> findAllByNameLike(String name, Pageable pageable);
+  @Query("{'name': { '$regex' : ?0, '$options' : 'i' }}")
+  Page<Actor> findAllByNameLike(String name, Pageable pageable);
 }
--- src/main/java/com/alan/movie/repository/MovieRepository.java
+++ src/main/java/com/alan/movie/repository/MovieRepository.java
@@ -4,9 +4,11 @@ import com.alan.movie.Model.Movie;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MovieRepository extends MongoRepository<Movie, String> {
-  public Page<Movie> findAllByNameLikeOrCode(String name, String code, Pageable pageable);
+  @Query("{'$or': [{'name': { '$regex' : ?0, '$options' : 'i' }}, {'code': { '$regex' : ?1, '$options' : 'i' }}]}")
+  Page<Movie> findAllByNameLikeOrCode(String name, String code, Pageable pageable);
 }
